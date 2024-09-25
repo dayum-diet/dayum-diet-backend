6ee1b1aa72c2f54c19a10761ce3238a461a2bce2
Daeho-Son <thseogh135@gmail.com>
2024-09-25 23:04:56 +0900

    init

---
+++ src/main/java/com/dayum/dayum_diet/DayumDietApplication.java
@@ -0,0 +1,13 @@
+package com.dayum.dayum_diet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayumDietApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayumDietApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dayum/dayum_diet/DayumDietApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dayum.dayum_diet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayumDietApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
