82e964089d954fe2e24baab0456ef4b0ef50d797
Daeho-Son <thseogh135@gmail.com>
2024-10-12 19:32:05 +0900

    fix: BaseEntity 적용이 되도록 제대로 설정
    [변경 사항]
    - JpaAuditingConfiguration.java
      `@EnableJpaAuditing`을 설정해서 JPA Auditing 활성화
    
    - BaseEntity.java
      `@EntityListeners(AuditingEntityListener.class)`를 추가해
      해당 클래스에 Auditing 적용
    
    해당 어노테이션을 추가해야 엔티티 생성 시,
        자동으로 create_at, modified_at이 업데이트 됨

---
+++ src/main/java/com/dayum/dayum_diet/common/config/JpaAuditingConfiguration.java
@@ -0,0 +1,9 @@
+package com.dayum.dayum_diet.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfiguration {
+}
